44df8bdb30025ea237d2b81ea8a3f987b18fca3e
RichardAPena <richardalexpena@hotmail.com>
2021-04-03 16:44:08 -0400

    two way file transfer

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -10,6 +10,8 @@ public class Client {
     private final static int PORT = 13337;
 
     private final static String FILENAME = "C:\\Downloads\\afafa.txt";
+    private final static String DIRECTORY = "C:\\Downloads\\CLIENT";
+
     public static void main(String[] args) throws IOException {
         Socket s = new Socket(HOST, PORT);
         //ClientHandler client = new ClientHandler(s); // TODO: idk make this do something
@@ -19,38 +21,59 @@ public class Client {
         //FileInputStream fin = new FileInputStream();
         //fin.read(b, 0, b.length);
 
-        InputStream is = s.getInputStream();
-        FileOutputStream fout = new FileOutputStream(FILENAME);
-        int arrLength = is.read();
-        System.out.println(arrLength);
-        byte[] b = new byte[arrLength];
-        is.read(b);
-        fout.write(b);
+//            InputStream is = s.getInputStream();
+//            FileOutputStream fout = new FileOutputStream(FILENAME);
+//            int arrLength = is.read();
+//            System.out.println(arrLength);
+//            byte[] b = new byte[arrLength];
+//            is.read(b);
+//            fout.write(b);
 
         Scanner scan = new Scanner(System.in);
         String keyboard = "";
 
-            /*
-            while (!keyboard.equals("quit")) {
+        while (!keyboard.equals("quit")) {
+                /*
+                 READ:
+                 DIR
+                 UPLOAD filename
+                 DOWNLOAD filename
+                */
+            keyboard = scan.nextLine();
+            out.println(keyboard);
+            if (keyboard.equals("DIR")) {
 
-                // READ:
-                // DIR
-                // UPLOAD filename
-                // DOWNLOAD filename
+                // Receive file list information from server
+                String fileList = in.readLine();
+                String[] fileArray = fileList.split(":");
+                for (String value : fileArray) System.out.println(value);
+            } else if (keyboard.startsWith("UPLOAD")) {
 
-                keyboard = scan.nextLine();
-                out.println(keyboard);
-                if (keyboard.equals("DIR")) {
-                    String fileList = in.readLine();
-                    String[] fileArray = fileList.split(":");
-                    for (String value : fileArray) System.out.println(value);
-                } else if (keyboard.startsWith("UPLOAD")) {
-                    System.out.println("Sending file: " + keyboard.replace("UPLOAD ", ""));
-                } else if (keyboard.startsWith("DOWNLOAD")) {
-                    System.out.println("Receiving file: " + keyboard.replace("DOWNLOAD ", ""));
-                }
+                // Send file to server
+                System.out.println("Sending file: " + keyboard.replace("UPLOAD ", ""));
+                File file = new File(keyboard.replace("UPLOAD ",""));
+                FileInputStream fin = new FileInputStream(keyboard.replace("UPLOAD ",""));
+                int length = (int) file.length();
+                byte[] fileBytes = new byte[length];
+                fin.read(fileBytes);
+                out.println(file.getName());
+                s.getOutputStream().write(length);
+                s.getOutputStream().write(fileBytes);
+                System.out.println("File sent");
+            } else if (keyboard.startsWith("DOWNLOAD")) {
+
+                // Receive file from server
+                System.out.println("Receiving file: " + keyboard.replace("DOWNLOAD ", ""));
+                String fileName = in.readLine();
+                FileOutputStream fout = new FileOutputStream(DIRECTORY+"\\"+fileName);
+                int length = s.getInputStream().read();
+                byte[] fileBytes = new byte[length];
+                s.getInputStream().read(fileBytes);
+                fout.write(fileBytes);
+                System.out.println("File received");
             }
+        }
 
-             */
+
     }
 }
--- src/sample/Server.java
+++ src/sample/Server.java
@@ -14,6 +14,7 @@ public class Server {
     private ExecutorService pool = Executors.newFixedThreadPool(4);
 
     private final static String FILENAME = "C:\\Downloads\\test.txt";
+    private final static String DIRECTORY = "C:\\Downloads\\SERVER";
 
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(PORT);
@@ -25,40 +26,62 @@ public class Server {
         //DataInputStream din = new DataInputStream(s.getInputStream());
         //DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
-        File file = new File(FILENAME);
-        FileInputStream fin = new FileInputStream(FILENAME);
+//        File file = new File(FILENAME);
+//        FileInputStream fin = new FileInputStream(FILENAME);
         //FileOutputStream fout = new FileOutputStream("");
 
-        int sus = (int) file.length();
-        byte[] b = new byte[sus];
-        fin.read(b, 0, b.length);
-        OutputStream os = s.getOutputStream();
-        os.write(b.length);
-        os.write(b, 0, b.length);
+//        int length = (int) file.length();
+//        byte[] b = new byte[length];
+//        fin.read(b, 0, b.length);
+//        OutputStream os = s.getOutputStream();
+//        os.write(b.length);
+//        os.write(b, 0, b.length);
 
-        System.out.println("FILE SENT");
+//        System.out.println("FILE SENT");
 
         String request = "";
 
-        /*
         while (!request.equals("quit")) {
-
-            // RESPOND TO:
-            // DIR
-            // UPLOAD filename
-            // DOWNLOAD filename
-
+            /*
+             RESPOND TO:
+             DIR
+             UPLOAD filename
+             DOWNLOAD filename
+            */
             request = in.readLine();
             System.out.println("REQUEST: " + request);
             if (request.equals("DIR")) {
+
+                // Send client file list information
                 out.println("bruh.txt:mihai.txt:e.png");
             } else if (request.startsWith("UPLOAD")) {
+
+                // Receive file from client
                 System.out.println("Receiving file: " + request.replace("UPLOAD ", ""));
+                // TODO: Receive file from client
+                String fileName = in.readLine();
+                FileOutputStream fout = new FileOutputStream(DIRECTORY+"\\"+fileName);
+                File file = new File(DIRECTORY+"\\"+fileName);
+                int length = (int) file.length();
+                byte[] fileBytes = new byte[length];
+                s.getInputStream().read(fileBytes);
+                fout.write(fileBytes);
+                System.out.println("File received");
             } else if (request.startsWith("DOWNLOAD")) {
+
+                // Send file to client
                 System.out.println("Sending file: " + request.replace("DOWNLOAD ", ""));
+                // TODO: Send file to client
+                File file = new File(request.replace("DOWNLOAD ",""));
+                FileInputStream fin = new FileInputStream(request.replace("DOWNLOAD ", ""));
+                int length = (int) file.length();
+                byte[] fileBytes = new byte[length];
+                fin.read(fileBytes);
+                out.println(file.getName());
+                s.getOutputStream().write(length);
+                s.getOutputStream().write(fileBytes);
+                System.out.println("File sent");
             }
         }
-
-         */
     }
 }
